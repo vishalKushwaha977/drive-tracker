d6d7142ba5d2079744a5fdcc2f643ce161dd8851
vishalKushwaha977 <59596343+vishalKushwaha977@users.noreply.github.com>
2020-02-29 15:22:14 +0530

    commit
    change the whole think

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/DriveTrackerAngular'),
+      dir: require('path').join(__dirname, './coverage/DriveTracker'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
